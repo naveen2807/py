af3a4475eb9c6ecead3ca7feb611c363a6950a83
naveen2807 <30363674+naveen2807@users.noreply.github.com>
2023-07-27 06:55:30 +0530

    Update p.py

--- p.py
+++ p.py
@@ -1,1 +1,23 @@
-print("Hello, World!")
+from __future__ import print_function  # Python 2/3 compatibility
+import json
+import boto3
+
+# AWS_ACCESS = ""
+# AWS_SECRET = ""
+AWS_REGION = "eu-west-3"
+TABLE_NAME = "users"
+
+client = boto3.client(
+    'dynamodb',
+    # aws_secret_access_key=AWS_SECRET,
+    # aws_access_key_id=AWS_ACCESS,
+    region_name=AWS_REGION)
+
+response = client.scan(
+    TableName=Music,
+    Select='ALL_ATTRIBUTES',
+    ReturnConsumedCapacity='TOTAL'
+)
+# print(json.dumps(response))
+with open(TABLE_NAME+'.json', 'w') as f:
+    print(json.dumps(response), file=f)
