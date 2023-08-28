04d3f5e999b24c97427a16c9fa91de298a078190
naveen2807 <30363674+naveen2807@users.noreply.github.com>
2023-08-28 12:27:08 +0530

    Create export.py

---
+++ export.py
@@ -0,0 +1,24 @@
+from __future__ import print_function  # Python 2/3 compatibility
+import json
+import boto3
+
+# AWS_ACCESS = ""
+# AWS_SECRET = ""
+AWS_REGION = "us-east-1"
+TABLE_NAME = "Test"
+
+client = boto3.client(
+     'dynamodb',
+     endpoint_url='http://localhost:8000', 
+     aws_secret_access_key='naveen',
+    aws_access_key_id='1234',
+    region_name=AWS_REGION)
+
+response = client.scan(
+    TableName=TABLE_NAME,
+    Select='ALL_ATTRIBUTES',
+    ReturnConsumedCapacity='TOTAL'
+)
+# print(json.dumps(response))
+with open('D:/sample/'+TABLE_NAME+'.json', 'w') as f:
+    print(json.dumps(response), file=f)
